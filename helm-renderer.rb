99a90f717d893ee1aa1b5df2995e2aa8c7826e87
Afnan Enayet <afnan@afnan.io>
2020-07-25 13:50:48 -0700

    Bump version, remove req
    * Update to latest version
    * Remove language/haskell requirements since this formula just downloads
      a static binary

--- helm-renderer.rb
+++ helm-renderer.rb
@@ -1,10 +1,8 @@
-require "language/haskell"
-
 class HelmRenderer < Formula
   desc "Render Helm charts for your k8s cluster locally"
   homepage "https://github.com/afnanenayet/helm-renderer"
-  url "https://github.com/afnanenayet/helm-renderer/releases/download/v0.1.6.0/helm-renderer-darwin.tar.gz"
-  sha256 "312239cf855c25f80aafad15ff0f2269a1d659acbfb749d395c4318bcbf81fe8"
+  url "https://github.com/afnanenayet/helm-renderer/releases/download/v0.1.7.0/helm-renderer-darwin.tar.gz"
+  sha256 "949b973c3507f8264f4df9d426895ab030bbd7ee6d7dd9ae9024ab271accdd13"
   head "https://github.com/afnanenayet/helm-renderer.git"
 
   def install
