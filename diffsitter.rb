8a3006e14f8135b1f0ccf2814ff0ee325da2ad5b
Afnan Enayet <afnan@afnan.io>
2021-01-16 21:15:22 -0600

    Add formula for diffsitter

---
+++ diffsitter.rb
@@ -0,0 +1,11 @@
+class DiffSitter < Formula
+  desc "Meaningful semantic diffs"
+  homepage "https://github.com/afnanenayet/diffsitter"
+  url "https://github.com/afnanenayet/diffsitter/releases/download/v0.1.1/diffsitter-darwin-amd64"
+  sha256 "19a83df11cb9ffac66f131cacfb09c4f60451f672468697d4117a289cb008d6a"
+  license "MIT"
+
+  def install
+    bin.install "diffsitter"
+  end
+end
