5fe9c04a4b2323ea9e3d539f86a7e38fd87d8f42
Afnan Enayet <afnan@afnan.io>
2021-01-24 23:35:29 -0600

    Update version variable
    Make the version a separate variable

--- diffsitter.rb
+++ diffsitter.rb
@@ -1,7 +1,8 @@
 class Diffsitter < Formula
   desc "Meaningful semantic diffs"
   homepage "https://github.com/afnanenayet/diffsitter"
-  url "https://github.com/afnanenayet/diffsitter/releases/download/v0.3.0-1/diffsitter-darwin-amd64.tar.gz"
+  version "0.3.0-1"
+  url "https://github.com/afnanenayet/diffsitter/releases/download/v#{version}/diffsitter-darwin-amd64.tar.gz"
   sha256 "33df21b1111ec060aca8033e5278dc8abaed9c95707f15b1ef60f4b514ec586b"
   license "MIT"
 
