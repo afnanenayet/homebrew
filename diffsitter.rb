e3784238b3b11be3c8918e9ee2b056b8feb7ac27
Afnan Enayet <afnan@afnan.io>
2021-01-31 23:03:33 -0600

    diffsitter v0.5.0_1

--- diffsitter.rb
+++ diffsitter.rb
@@ -1,12 +1,13 @@
 class Diffsitter < Formula
   desc "Meaningful semantic diffs"
   homepage "https://github.com/afnanenayet/diffsitter"
-  version "0.5.0"
+  version "0.5.0_1"
   url "https://github.com/afnanenayet/diffsitter/releases/download/v#{version}/diffsitter-darwin-amd64.tar.gz"
-  sha256 "094d17b6175e7db051b824d014b12b63fcd27570d47d48ddce2972c28268f672"
+  sha256 "e4b2fc9e877f5611c00ba25f577e2b0557e58408e81896f85c61125dd91b9613"
   license "MIT"
 
   def install
     bin.install "diffsitter"
+    bin.install "git-diffsitter"
   end
 end
