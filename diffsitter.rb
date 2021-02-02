0fa99ac66de525a03bc2860d9cf7eafe1634c0ff
Afnan Enayet <afnan@afnan.io>
2021-02-02 08:49:20 -0600

    Bump version, fix `brew audit` problems
    * Bump version to a new schema that conforms to homebrew's version
      guidelines
    * Bring back `git-diffsitter`
    * Fix issues pointed out by brew audit

--- diffsitter.rb
+++ diffsitter.rb
@@ -1,12 +1,13 @@
 class Diffsitter < Formula
-  desc "A tree-sitter based AST difftool to get meaningful semantic diffs"
-  homepage ""
-  version "0.5.0_1"
+  desc "Tree-sitter based AST difftool to get meaningful semantic diffs"
+  homepage "https://github.com/afnanenayet/diffsitter"
   url "https://github.com/afnanenayet/diffsitter/releases/download/v#{version}/diffsitter-darwin-amd64.tar.gz"
+  version "0.5.0-1"
   sha256 "e4b2fc9e877f5611c00ba25f577e2b0557e58408e81896f85c61125dd91b9613"
   license "MIT"
 
   def install
     bin.install "diffsitter"
+    bin.install "git-diffsitter"
   end
 end
