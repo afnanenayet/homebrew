a66a0dabdfc03d7814d8d664b50d7d48f8fbfb76
Afnan Enayet <afnan@afnan.io>
2021-02-02 06:52:11 -0600

    Use explicit version again

--- diffsitter.rb
+++ diffsitter.rb
@@ -1,7 +1,8 @@
 class Diffsitter < Formula
   desc "A tree-sitter based AST difftool to get meaningful semantic diffs"
   homepage ""
-  url "https://github.com/afnanenayet/diffsitter/releases/download/v0.5.0_1/diffsitter-darwin-amd64.tar.gz"
+  version = "0.5.0_1"
+  url "https://github.com/afnanenayet/diffsitter/releases/download/v#{version}/diffsitter-darwin-amd64.tar.gz"
   sha256 "e4b2fc9e877f5611c00ba25f577e2b0557e58408e81896f85c61125dd91b9613"
   license "MIT"
 
