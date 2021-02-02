482f4b5a6d9bc0f3a32c448ff2f785c8233e84af
Afnan Enayet <afnan@afnan.io>
2021-02-02 08:52:09 -0600

    Fix string interpolation issue

--- diffsitter.rb
+++ diffsitter.rb
@@ -1,8 +1,8 @@
 class Diffsitter < Formula
   desc "Tree-sitter based AST difftool to get meaningful semantic diffs"
   homepage "https://github.com/afnanenayet/diffsitter"
-  url "https://github.com/afnanenayet/diffsitter/releases/download/v#{version}/diffsitter-darwin-amd64.tar.gz"
   version "0.5.0-1"
+  url "https://github.com/afnanenayet/diffsitter/releases/download/v#{version}/diffsitter-darwin-amd64.tar.gz"
   sha256 "e4b2fc9e877f5611c00ba25f577e2b0557e58408e81896f85c61125dd91b9613"
   license "MIT"
 
