c91a91214194714a1b2a710b826aa1a5850823bf
Afnan Enayet <afnan@afnan.io>
2021-02-16 07:28:11 -0600

    [chore] diffsitter v0.6.1

--- diffsitter.rb
+++ diffsitter.rb
@@ -1,9 +1,9 @@
 class Diffsitter < Formula
   desc "Tree-sitter based AST difftool to get meaningful semantic diffs"
   homepage "https://github.com/afnanenayet/diffsitter"
-  version "0.5.0-1"
+  version "0.6.1"
   url "https://github.com/afnanenayet/diffsitter/releases/download/v#{version}/diffsitter-darwin-amd64.tar.gz"
-  sha256 "9a768601b3cb52b802654c8310a60bea74b02213980ab7e27fdcf75dbb328a8f"
+  sha256 "e5753d1587aa928b0c842229325e1214a923da3b0fed359266cb57388ce93b10"
   license "MIT"
 
   def install
