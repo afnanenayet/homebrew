787d91bb8f5f9cd282988f82e4ba23c963c442bb
Afnan Enayet <afnan@afnan.io>
2021-05-01 20:17:49 -0500

    Fix version on formula

--- diffsitter.rb
+++ diffsitter.rb
@@ -1,6 +1,7 @@
 class Diffsitter < Formula
   desc "Tree-sitter based AST difftool to get meaningful semantic diffs"
   homepage "https://github.com/afnanenayet/diffsitter"
+  version "0.6.6"
   url "https://github.com/afnanenayet/diffsitter/releases/download/v0.6.6/diffsitter-x86_64-apple-darwin.tar.gz"
   sha256 "f05e0e4b242e7501416dc1cb2fd6853c56ef8a95eb576ad9d6091b58a2782bb6"
   license "MIT"
