79c588f25e60a2220b47ba93f8d47238b090401c
Afnan Enayet <afnan@afnan.io>
2021-11-28 14:05:18 -0500

    Fix checksum

--- diffsitter.rb
+++ diffsitter.rb
@@ -3,7 +3,7 @@ class Diffsitter < Formula
   homepage "https://github.com/afnanenayet/diffsitter"
   version "0.6.7"
   url "https://github.com/afnanenayet/diffsitter/releases/download/v0.6.7/diffsitter-x86_64-apple-darwin.tar.gz"
-  sha256 "$shachecksum"
+  sha256 "100f40fb4d02a694caa845fea5bad98412b8e00c57ce9a2d0628888d2c87b636"
   license "MIT"
 
   def install
