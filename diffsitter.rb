81325996ac0e1a1d2dd5d521932a7771da63420f
afnanenayet <afnanenayet@users.noreply.github.com>
2023-05-10 04:26:11 +0000

    Update diffsitter package formula to v0.8.0

--- diffsitter.rb
+++ diffsitter.rb
@@ -1,13 +1,1 @@
-class Diffsitter < Formula
-  desc "Tree-sitter based AST difftool to get meaningful semantic diffs"
-  homepage "https://github.com/afnanenayet/diffsitter"
-  version "0.7.2"
-  url "https://github.com/afnanenayet/diffsitter/releases/download/v0.7.2/diffsitter-x86_64-apple-darwin.tar.gz"
-  sha256 "ac57bf00e2d6cc5c532650140b9e31024432943b4e9603c65db51a8a3463fb18"
-  license "MIT"
-
-  def install
-    bin.install "diffsitter"
-    bin.install "git-diffsitter"
-  end
-end
+Not Found
