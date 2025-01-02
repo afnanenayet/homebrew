fb91365a8c4df3e9ae55e711efd71a36cbd8c1c7
Afnan Enayet <afnanenayet@users.noreply.github.com>
2025-01-01 20:19:13 -0500

    chore: Bump diffsitter version (#16)
    Fixes an issue raised in the main diffsitter repo.

--- diffsitter.rb
+++ diffsitter.rb
@@ -7,13 +7,13 @@ class Diffsitter < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/afnanenayet/diffsitter/releases/download/v0.8.1/diffsitter-aarch64-apple-darwin.tar.gz"
-      sha256 "c6384fd5d4afd9c2212bc1f89812a926335cd50d47cc8af3e26b30975e072885"
+      url "https://github.com/afnanenayet/diffsitter/releases/download/v0.8.4/diffsitter-aarch64-apple-darwin.tar.gz"
+      sha256 "b2aa50554b768370e698e30c17723b855909240f93fd01492bafb251ff680478"
     end
 
     on_intel do
-      url "https://github.com/afnanenayet/diffsitter/releases/download/v0.8.1/diffsitter-x86_64-apple-darwin.tar.gz"
-      sha256 "5e27db1d6a8acd492b19cdee4cd3be57f7d6c4d6b4db5685c620d1a3e5d8be6b"
+      url "https://github.com/afnanenayet/diffsitter/releases/download/v0.8.4/diffsitter-x86_64-apple-darwin.tar.gz"
+      sha256 "b491654004019a6422c7cdb730a44c31d867841ba69908c214f01942535fc943"
     end
   end
 
